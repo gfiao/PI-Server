c0eb201e48746e3640d08b40ce0fd52cc40740f6
Rafael Bizarra <r.bizarra@campus.fct.unl.pt>
2014-05-14 15:12:34 +0100

    [Server] Header/Gemfile alterados

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'devise'
+
+gem 'youtube_it'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+
   get 'homepage/index'
 
-
-
+  root 'homepage#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,4 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
