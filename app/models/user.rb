4858e70bebed2e360622adcd34c3bd4cdd9e16a0
Gui <g.fiao@campus.fct.unl.pt>
2014-05-21 15:50:05 +0100

    [Website] Página de perfil

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   has_many :free_classrooms
   #has_many :classrooms, through: :free_classrooms
 
-  has_many :games, through: :scores
+  has_many :scores
+  #has_many :games, through: :scores
 
   validate :name, presence: true;
   validate :gender, presence: true;
