891d308ec005ab16c09c44453b72573b0c808b7e
Gui <g.fiao@campus.fct.unl.pt>
2014-05-26 14:34:01 +0100

    [Website] Edit user
    Página para os utilizadores mudarem o seu perfil está feito, falta
    aplicar CSS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    registration_params = [:name, :email, :password, :password_confirmation, :birth_date, :about_me, :gender]
+    registration_params = [:name, :email, :password, :password_confirmation, :birth_date, :about_me, :gender, :course]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) {
