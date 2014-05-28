65e0f9ca31d135d6b72003465dd079b874920db9
leofiao <loturcofiao@gmail.com>
2014-05-28 17:47:07 +0100

    [Cenas] Resolver conflito

--- public/javascripts/trabalho.js
+++ public/javascripts/trabalho.js
@@ -3,6 +3,11 @@
  */
 
 
+function init() {
+
+    populateFooter();
+}
+
 
 //função que adapta uma div à imagem que tiver dentro dela
 function resizeText() {
@@ -10,4 +15,54 @@ function resizeText() {
 }
 
 
+jQuery.extend({
+    getValues: function (url) {
+        var result = null;
+        $.ajax({
+            url: url,
+            type: 'get',
+            dataType: 'json',
+            async: false,
+            cache: false,
+            success: function (data) {
+                result = data;
+            }
+        });
+        return result;
+    }
+});
 
+function populateHeadlineDescription() {
+    var interval_ID;
+    var obj;
+    var contents = $.getValues('/contents')
+
+    interval_ID = setInterval(function () {
+
+        //id da posição seleccionada actualmente
+        obj = $("#news_carousel .active").attr('id');
+
+        var splitted_id = obj.split("_");
+        var id = splitted_id[splitted_id.length - 1]; //obtem o ID e normaliza-o para 0 até length-1
+
+        var toChange = $("#sideDescription > h2");
+        toChange.empty();
+        toChange.append(contents[id].title);
+
+        toChange = $("#sideDescription > p");
+        toChange.empty();
+        toChange.append(contents[id].description);
+
+        clearInterval(interval_ID);
+    }, 650);
+
+}
+
+function populateFooter() {
+
+    var rodape = $.getValues('/footer_news');
+    console.log(rodape);
+
+
+
+}
