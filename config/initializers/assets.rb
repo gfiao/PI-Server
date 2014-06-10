27b0a746a0f6d7009468640871a5cc75823a6dd1
leofiao <loturcofiao@gmail.com>
2014-06-10 15:53:20 +0100

    [Jogo] Jogo 2048 inserido no site
    Versão inicial, so tem as mesmas funcionalidades que o jogo original

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,17 @@ Rails.application.config.assets.precompile += %w( tv.css )
 Rails.application.config.assets.precompile += %w( mediaelementplayer.css )
 Rails.application.config.assets.precompile += %w( animate.css )
 Rails.application.config.assets.precompile += %w( tv.js )
+
+#JS e CSS para 2048
+Rails.application.config.assets.precompile += %w( bind_polyfill.js )
+Rails.application.config.assets.precompile += %w( classlist_polyfill.js )
+Rails.application.config.assets.precompile += %w( animframe_polyfill.js )
+Rails.application.config.assets.precompile += %w( keyboard_input_manager.js )
+Rails.application.config.assets.precompile += %w( html_actuator.js )
+Rails.application.config.assets.precompile += %w( grid.js )
+Rails.application.config.assets.precompile += %w( tile.js )
+Rails.application.config.assets.precompile += %w( local_storage_manager.js )
+Rails.application.config.assets.precompile += %w( game_manager.js )
+Rails.application.config.assets.precompile += %w( application2048.js )
+
+Rails.application.config.assets.precompile += %w( main2048.css )
